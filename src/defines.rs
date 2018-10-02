4ca9096fb316786796e1c868980bf6fafc230c78
zengjirui <zengjirui@qq.com>
2018-10-02 17:40:59 +0800

    process window events

--- src/defines.rs
+++ src/defines.rs
@@ -1,63 +1,69 @@
 #![allow(unused)]
-pub const   MSG_OUT_CLOSE: u32              = 0x00;
-pub const   MSG_OUT_STATS: u32              = 0x01;
-pub const   MSG_OUT_PUTS: u32               = 0x02;
-pub const   MSG_OUT_WRITE: u32              = 0x03;
-pub const   MSG_OUT_INSPECT: u32            = 0x04;
-pub const   MSG_OUT_RESHAPE: u32            = 0x05;
-pub const   MSG_OUT_READY: u32              = 0x06;
-pub const   MSG_OUT_DRAW_READY: u32         = 0x07;
+pub const MSG_OUT_CLOSE: u32 = 0x00;
+pub const MSG_OUT_STATS: u32 = 0x01;
+pub const MSG_OUT_PUTS: u32 = 0x02;
+pub const MSG_OUT_WRITE: u32 = 0x03;
+pub const MSG_OUT_INSPECT: u32 = 0x04;
+pub const MSG_OUT_RESHAPE: u32 = 0x05;
+pub const MSG_OUT_READY: u32 = 0x06;
+pub const MSG_OUT_DRAW_READY: u32 = 0x07;
 
-pub const   MSG_OUT_KEY: u32                = 0x0A;
-pub const   MSG_OUT_CODEPOINT: u32          = 0x0B;
-pub const   MSG_OUT_CURSOR_POS: u32         = 0x0C;
-pub const   MSG_OUT_MOUSE_BUTTON: u32       = 0x0D;
-pub const   MSG_OUT_MOUSE_SCROLL: u32       = 0x0E;
-pub const   MSG_OUT_CURSOR_ENTER: u32       = 0x0F;
-pub const   MSG_OUT_DROP_PATHS: u32         = 0x10;
-pub const   MSG_OUT_CACHE_MISS: u32         = 0x20;
+pub const MSG_OUT_KEY: u32 = 0x0A;
+pub const MSG_OUT_CODEPOINT: u32 = 0x0B;
+pub const MSG_OUT_CURSOR_POS: u32 = 0x0C;
+pub const MSG_OUT_MOUSE_BUTTON: u32 = 0x0D;
+pub const MSG_OUT_MOUSE_SCROLL: u32 = 0x0E;
+pub const MSG_OUT_CURSOR_ENTER: u32 = 0x0F;
+pub const MSG_OUT_DROP_PATHS: u32 = 0x10;
+pub const MSG_OUT_CACHE_MISS: u32 = 0x20;
 
-pub const   MSG_OUT_FONT_MISS: u32          = 0x22;
+pub const MSG_OUT_FONT_MISS: u32 = 0x22;
 
 // pub const   MSG_OUT_NEW_DL_ID: u32          = 0x30;
-pub const   MSG_OUT_NEW_TX_ID: u32          = 0x31;
-pub const   MSG_OUT_NEW_FONT_ID: u32        = 0x32;
+pub const MSG_OUT_NEW_TX_ID: u32 = 0x31;
+pub const MSG_OUT_NEW_FONT_ID: u32 = 0x32;
 
-
-
-pub const   CMD_RENDER_GRAPH: u32           = 0x01;
-pub const   CMD_CLEAR_GRAPH: u32            = 0x02;
-pub const   CMD_SET_ROOT: u32               = 0x03;
+pub const CMD_RENDER_GRAPH: u32 = 0x01;
+pub const CMD_CLEAR_GRAPH: u32 = 0x02;
+pub const CMD_SET_ROOT: u32 = 0x03;
 // pub const   CMD_CACHE_LOAD: u32             = 0x03;
 // pub const   CMD_CACHE_RELEASE: u32          = 0x04;
 
-pub const   CMD_CLEAR_COLOR: u32            = 0x05;
+pub const CMD_CLEAR_COLOR: u32 = 0x05;
 
-pub const   CMD_INPUT: u32                  = 0x0A;
+pub const CMD_INPUT: u32 = 0x0A;
 
-pub const   CMD_QUIT: u32                   = 0x20;
-pub const   CMD_QUERY_STATS: u32            = 0x21;
-pub const   CMD_RESHAPE: u32                = 0x22;
-pub const   CMD_POSITION: u32               = 0x23;
-pub const   CMD_FOCUS: u32                  = 0x24;
-pub const   CMD_ICONIFY: u32                = 0x25;
-pub const   CMD_MAXIMIZE: u32               = 0x26;
-pub const   CMD_RESTORE: u32                = 0x27;
-pub const   CMD_SHOW: u32                   = 0x28;
-pub const   CMD_HIDE: u32                   = 0x29;
+pub const CMD_QUIT: u32 = 0x20;
+pub const CMD_QUERY_STATS: u32 = 0x21;
+pub const CMD_RESHAPE: u32 = 0x22;
+pub const CMD_POSITION: u32 = 0x23;
+pub const CMD_FOCUS: u32 = 0x24;
+pub const CMD_ICONIFY: u32 = 0x25;
+pub const CMD_MAXIMIZE: u32 = 0x26;
+pub const CMD_RESTORE: u32 = 0x27;
+pub const CMD_SHOW: u32 = 0x28;
+pub const CMD_HIDE: u32 = 0x29;
 
 // pub const   CMD_NEW_DL_ID: u32              = 0x30;
 // pub const   CMD_FREE_DL_ID: u32             = 0x31;
 
-pub const   CMD_NEW_TX_ID: u32              = 0x32;
-pub const   CMD_FREE_TX_ID: u32             = 0x33;
-pub const   CMD_PUT_TX_BLOB: u32            = 0x34;
-pub const   CMD_PUT_TX_RAW: u32             = 0x35;
+pub const CMD_NEW_TX_ID: u32 = 0x32;
+pub const CMD_FREE_TX_ID: u32 = 0x33;
+pub const CMD_PUT_TX_BLOB: u32 = 0x34;
+pub const CMD_PUT_TX_RAW: u32 = 0x35;
 
-
-pub const   CMD_LOAD_FONT_FILE: u32         = 0x37;
-pub const   CMD_LOAD_FONT_BLOB: u32         = 0x38;
-pub const   CMD_FREE_FONT: u32              = 0x39;
+pub const CMD_LOAD_FONT_FILE: u32 = 0x37;
+pub const CMD_LOAD_FONT_BLOB: u32 = 0x38;
+pub const CMD_FREE_FONT: u32 = 0x39;
 
 // here to test recovery
-pub const   CMD_CRASH: u32                  = 0xFE;
+pub const CMD_CRASH: u32 = 0xFE;
+
+pub const MSG_KEY_MASK: u32 = 0x0001;
+pub const MSG_CHAR_MASK: u32 = 0x0002;
+pub const MSG_MOUSE_MOVE_MASK: u32 = 0x0004;
+pub const MSG_MOUSE_BUTTON_MASK: u32 = 0x0008;
+pub const MSG_MOUSE_SCROLL_MASK: u32 = 0x0010;
+pub const MSG_MOUSE_ENTER_MASK: u32 = 0x0020;
+pub const MSG_DROP_PATHS_MASK: u32 = 0x0040;
+pub const MSG_RESHAPE_MASK: u32 = 0x0080;
--- src/event.rs
+++ src/event.rs
@@ -1,17 +1,82 @@
 use comms::*;
-use types::*;
+use defines::*;
 use glfw::WindowEvent;
-pub fn handle_window_event(window_data: &mut WindowData, event: WindowEvent){
-
+use types::*;
+pub fn handle_window_event(window_data: &mut WindowData, event: WindowEvent) {
+    match event {
+        WindowEvent::FramebufferSize(w, h) => reshape_framebuffer(window_data, w, h),
+        WindowEvent::Size(w, h) => reshape_window(window_data, w, h),
+        WindowEvent::Key(key, scancode, action, mods) => {
+            key_callback(
+                window_data,
+                key as i32,
+                scancode,
+                action as i32,
+                mods.bits(),
+            );
+        }
+        WindowEvent::CharModifiers(codepoint, mods) => {
+            charmods_callback(window_data, codepoint as u32, mods.bits());
+        }
+        WindowEvent::CursorPos(xpos, ypos) => cursor_pos_callback(window_data, xpos, ypos),
+        WindowEvent::MouseButton(button, action, mods) => {
+            mouse_button_callback(window_data, button as i32, action as i32, mods.bits())
+        }
+        WindowEvent::Scroll(xoffset, yoffset) => {
+            scroll_callback(window_data, xoffset, yoffset);
+        }
+        WindowEvent::CursorEnter(entered) => cursor_enter_callback(window_data, entered as i32),
+        WindowEvent::Close => window_close_callback(window_data),
+        _ => (),
+    };
 }
-
-pub fn reshape_framebuffer(window: &mut WindowData, width: i32, height: i32) {
-    window.frame_size = (width, height);
-    window.redraw = true;
-    
+pub fn key_callback(window_data: &mut WindowData, key: i32, scancode: i32, action: i32, mods: i32) {
+    if window_data.input_flags & MSG_KEY_MASK != 0 {
+        send_key(key, scancode, action, mods);
+    }
 }
-pub fn reshape_window(window: &mut WindowData, width: i32, height: i32) {
-    window.window_size = (width, height);
+pub fn charmods_callback(window_data: &mut WindowData, codepoint: u32, mods: i32) {
+    if window_data.input_flags & MSG_CHAR_MASK != 0 {
+        send_codepoint(codepoint, mods);
+    }
+}
+pub fn cursor_pos_callback(window_data: &mut WindowData, xpos: f64, ypos: f64) {
+    let pos = (xpos as f32, ypos as f32);
+    if window_data.last_cursor_pos != pos
+    //&& window_data.input_flags & MSG_MOUSE_MOVE_MASK != 0
+    {
+        send_cursor_pos(pos.0, pos.1);
+        window_data.last_cursor_pos = pos;
+    }
+}
+pub fn mouse_button_callback(window_data: &mut WindowData, button: i32, action: i32, mods: i32) {
+    if window_data.input_flags & MSG_MOUSE_BUTTON_MASK != 0 {
+        let (x, y) = window_data.window.get_cursor_pos();
+        send_mouse_button(button, action, mods, x as f32, y as f32);
+    }
+}
+pub fn scroll_callback(window_data: &mut WindowData, xoffset: f64, yoffset: f64) {
+    if window_data.input_flags & MSG_MOUSE_SCROLL_MASK != 0 {
+        let (x, y) = window_data.window.get_cursor_pos();
+        send_scroll(xoffset as f32, yoffset as f32, x as f32, y as f32);
+    }
+}
+pub fn cursor_enter_callback(window_data: &mut WindowData, entered: i32) {
+    if window_data.input_flags & MSG_MOUSE_ENTER_MASK != 0 {
+        let (x, y) = window_data.window.get_cursor_pos();
+        send_cursor_enter(entered, x as f32, y as f32);
+    }
+}
+pub fn window_close_callback(window_data: &mut WindowData) {
+    send_close();
+    window_data.window.set_should_close(false);
+}
+pub fn reshape_framebuffer(window_data: &mut WindowData, width: i32, height: i32) {
+    window_data.frame_size = (width, height);
+    window_data.redraw = true;
+}
+pub fn reshape_window(window_data: &mut WindowData, width: i32, height: i32) {
+    window_data.window_size = (width, height);
     send_reshape(width, height, width, height);
-    window.redraw = true;
+    window_data.redraw = true;
 }
